5f50561380432c144ddb0aafe19f3467ce7590c7
Parth Mehta <tech@iamdifferent.io>
2018-09-24 17:25:29 +1000

    :fire: removed unnecessary code

--- templates/src/App/index.js
+++ templates/src/App/index.js
@@ -4,15 +4,15 @@ import chillifront from 'chillifront';
 /** Components */
 import Entry from './Entry';
 
-/** Helpers */
-import configureStore from '../store';
-
 /** App */
 const App = chillifront(
 	[
 		/* Mods go here */
 	],
-	configureStore
+	{
+		useDevTools: true,
+		debug: true,
+	}
 )(Entry);
 
 export default App;
--- templates/src/store/index.d.ts
+++
@@ -1,6 +0,0 @@
-export default function configureStore(
-	initialState: AppState,
-	modReducers: Object,
-	modMiddleware: Function[],
-	modStoreEnhancers: Function[]
-): Function;
